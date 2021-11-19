df00c899fbb0294cff53c726b6dde1f72927227b
Никитин Валерий Александрович <Nikitin@vmz.local>
2021-11-19 11:20:30 +0300

    Подготовка окружения для Page Object

---
+++ conftest.py
@@ -0,0 +1,29 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+ 
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="firefox",
+                     help = "Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="en",
+                     help = "Choose language: ar,ca,es,da,de,en-gb,el,es,fi,fr,it,ko,nl,pl,pt,pt-br,ro,ru,sk,uk,zh-hans")
+@pytest.fixture(scope="function")
+def browser(request):
+    print("\nstart browser for test..")
+    browser_name = request.config.getoption("browser_name")
+    language = request.config.getoption("language")
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+    if browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages",language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")     
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
