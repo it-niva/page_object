1fd0dad81d5ee941fafe189db6df67e064dc42d1
Nikitin Valery <nikitin3valery@gmail.com>
2021-11-25 11:18:55 +0300

    явная инициализация LoginPage страниц в теле теста

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,8 +1,13 @@
 from .base_page import BasePage
+from .locators import MainPageLocators
+from .login_page import LoginPage
 from selenium.webdriver.common.by import By
 
 class MainPage(BasePage): 
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+        #return LoginPage(browser = self.browser, url = self.browser.current_url)
+        #login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        #login_link.click()
         
