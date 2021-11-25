d1eaedd1e7024f66c5c0aa622312432290fac8db
Nikitin Valery <nikitin3valery@gmail.com>
2021-11-25 11:41:46 +0300

    явная инициализация LoginPage страниц в теле теста проверками should_be_login_url..form..registration_form

--- pages/locators.py
+++ pages/locators.py
@@ -5,5 +5,9 @@ class MainPageLocators():
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
-    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
 
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "login_link_inc")
+    
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
-
+from .locators import LoginPageLocators
+from selenium.webdriver.common.by import By
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -8,13 +9,13 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
+        assert "login" in self.browser.current_url, "This is not URL for login" # реализуйте проверку на корректный url адрес
         assert True
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not present" # реализуйте проверку, что есть форма логина
         assert True
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Registration form is not present" # реализуйте проверку, что есть форма регистрации на странице
         assert True
